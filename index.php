719968bcbd9f7ad04e6c5addb08d007ef4daa08c
genius <genius21513@gmail.com>
2022-12-27 04:44:59 +0900

    with form

--- index.php
+++ index.php
@@ -1,28 +1,49 @@
+
+
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-require_once 'vendor/autoload.php';
+    require_once 'vendor/autoload.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    
+    if(isset($_POST['submit'])){
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.office365.com';
+        $mail->Port       = 587;
+        $mail->SMTPSecure = 'tls';        
+        $mail->SMTPAuth   = true;
+        $mail->Username = 'geniusdev1011@outlook.com';
+        $mail->Password = 'Strong123.!@#';
+        $mail->SetFrom('geniusdev1011@outlook.com', 'FromEmail');
+        $mail->addAddress($_POST['email'], 'ToEmail');
+        //$mail->SMTPDebug  = 3;
+        //$mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";}; //$mail->Debugoutput = 'echo';
+        $mail->IsHTML(true);
 
-$mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->Host = 'smtp.office365.com';
-$mail->Port       = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth   = true;
-$mail->Username = 'your-outlook-email-address';
-$mail->Password = 'your-outlook-password';
-$mail->SetFrom('your-outlook-email-address( = username)', 'FromEmail');
-$mail->addAddress('receiver-user-email', 'ToEmail');
-//$mail->SMTPDebug  = 3;
-//$mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";}; //$mail->Debugoutput = 'echo';
-$mail->IsHTML(true);
+        $mail->Subject = 'This is Subject';
+        $mail->Body    = $_POST['message'];
+        $mail->AltBody = 'This is Body text.';
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is authenticated by **** user';
-$mail->AltBody = 'Authentication user and send user should be equal. This is test message, so understand issue from outlook';
+        if(!$mail->send()) {
+            echo '<h2>Message could not be sent.</h2>';
+            echo '<h2>Mailer Error: ' . $mail->ErrorInfo . '</h2>';
+        } else {
+            echo '<h2>Message has been sent successfully.</h2>';
+        }
+    }
+?>
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="" method="post">
+This is my email sender { geniusdev1011@outlook.com } <br>
+To Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
