3008468ac1c15197b1e09f1e43507e9a09018b76
genius <genius21513@gmail.com>
2022-12-27 03:00:28 +0900

    Send local e-mail by phpmailer

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once 'vendor/autoload.php';
+
+//PHPMailer Object
+$mail = new PHPMailer( true );
+//Argument true in constructor enables exceptions
+
+//From email address and name
+$mail->From = 'from@yourdomain.com';
+$mail->FromName = 'Full Name';
+
+//To address and name
+$mail->addAddress( 'recepient1@example.com', 'Recepient Name' );
+$mail->addAddress( 'recepient1@example.com' );
+//Recipient name is optional
+
+//Address to which recipient will reply
+$mail->addReplyTo( 'reply@yourdomain.com', 'Reply' );
+
+//CC and BCC
+$mail->addCC( 'cc@example.com' );
+$mail->addBCC( 'bcc@example.com' );
+
+//Send HTML or Plain Text email
+$mail->isHTML( true );
+
+$mail->Subject = 'Subject Text';
+$mail->Body = '<i>Mail body in HTML</i>';
+$mail->AltBody = 'This is the plain text version of the email content';
+
+try {
+    $mail->send();
+    echo 'Message has been sent successfully';
+} catch ( Exception $e ) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
